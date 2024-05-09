a5358e3a7671b386db744a1a39d4ce98a44bd98d
SuryakrishnanGunasekaran1 <169053660+SuryakrishnanGunasekaran1@users.noreply.github.com>
2024-05-09 09:40:01 +0530

    Add files via upload

---
+++ demotypesblock.java
@@ -0,0 +1,19 @@
+class superdemo
+{
+    {
+        System.out.println("hi from instant block");
+    }
+    
+    static{
+        System.out.println("hi from static block");
+    }
+}
+
+
+class demoblocktypes
+{
+    public static void main (String[] args) {
+        superdemo sd = new superdemo();
+        
+    }
+}

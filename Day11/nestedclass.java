0fcce411d3aa203f5cc3c267d13236a97a5d1ed0
SuryakrishnanGunasekaran1 <169053660+SuryakrishnanGunasekaran1@users.noreply.github.com>
2024-05-27 11:11:59 +0530

    Add files via upload

---
+++ Day11/nestedclass.java
@@ -0,0 +1,30 @@
+class SASTRA
+{
+    String town = "THANJAVUR";
+    String area = "VALLAM";
+    int pincode = 612204;
+    class SRC
+
+    {
+        String C_area = "KUMBAKONAM";
+        String C_street = "MOTHILAL street";
+        void displaydetails()
+        {
+            System.out.println(town);
+            System.out.println(area);
+            System.out.println(pincode);
+            System.out.println(C_area + " " + C_street);
+        }
+
+    }
+}
+
+
+public class nestedclass {
+    public static void main(String[] args) {
+        SASTRA t = new SASTRA();
+        SASTRA.SRC k = t.new SRC();
+        k.displaydetails();
+    }
+    
+}
